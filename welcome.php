32b9ac6709a8216faf076f34d838c90608a787bd
diaquino <25682@ma-web.nl>
2020-04-22 23:57:11 +0200

    indent

--- welcome.php
+++ welcome.php
@@ -1,31 +1,29 @@
 <html>
-<body>
+  <body>
+    <?php
+      // define variables and set to empty values
+      $name = $email = $telephone = $website = "";
 
-<?php
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = input_filter($_POST["name"]);
+        $email = input_filter($_POST["email"]);
+        $telephone = input_filter($_POST["telephone"]);
+        $website = input_filter($_POST["website"]);
+      }
 
-// define variables and set to empty values
-$name = $email = $telephone = $website = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = input_filter($_POST["name"]);
-  $email = input_filter($_POST["email"]);
-  $telephone = input_filter($_POST["telephone"]);
-  $website = input_filter($_POST["website"]);
-}
-
-function input_filter($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-
-?>
+      function input_filter($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+    ?>
 
     <p>Welkom <?php echo $name; ?></p>
     <p>E-mail: <?php echo $email; ?></p>
     <p>Telefoon: <?php echo $telephone; ?></p>
     <p>Website: <a href="<?php echo $website; ?>"><?php echo $website; ?></a></p>
 
-</body>
+    <script src="js/script.js"></script>
+  </body>
 </html>
